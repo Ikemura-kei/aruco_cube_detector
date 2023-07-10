9e0e3c2a828600642ef9962cbc8fa70e0e1aa23b
IKEMURA <ikemurakei2001@gmail.come>
2023-07-10 15:03:40 +0800

    finished communication with MCU, also added disconnection logics

--- scripts/aruco_cube_detector_node.py
+++ scripts/aruco_cube_detector_node.py
@@ -3,6 +3,62 @@ import numpy as np
 import rospy
 import os
 from geometry_msgs.msg import PoseStamped
+import struct
+import serial
+import crc16
+import time
+import subprocess
+
+# struct Pose
+# {
+#     // Position
+#     float x;
+#     float y;
+#     float z;
+#     // Quaternion
+#     float q;
+#     float i;
+#     float j;
+#     float k;
+# } __attribute__((packed));
+
+# struct CVMessage
+# {
+#     Pose headPose;
+#     Pose cubePose;
+#     uint16_t crc;
+# } __attribute__((packed));
+
+def crc16(data: bytes):
+    '''
+    CRC-16 (CCITT) implemented with a precomputed lookup table
+    '''
+    table = [ 
+    0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf, 0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7, 0x1081, 0x0108,
+    0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e, 0x9cc9, 0x8d40, 0xbfdb, 0xae52, 0xdaed, 0xcb64, 0xf9ff, 0xe876, 0x2102, 0x308b, 0x0210, 0x1399,
+    0x6726, 0x76af, 0x4434, 0x55bd, 0xad4a, 0xbcc3, 0x8e58, 0x9fd1, 0xeb6e, 0xfae7, 0xc87c, 0xd9f5, 0x3183, 0x200a, 0x1291, 0x0318, 0x77a7, 0x662e,
+    0x54b5, 0x453c, 0xbdcb, 0xac42, 0x9ed9, 0x8f50, 0xfbef, 0xea66, 0xd8fd, 0xc974, 0x4204, 0x538d, 0x6116, 0x709f, 0x0420, 0x15a9, 0x2732, 0x36bb,
+    0xce4c, 0xdfc5, 0xed5e, 0xfcd7, 0x8868, 0x99e1, 0xab7a, 0xbaf3, 0x5285, 0x430c, 0x7197, 0x601e, 0x14a1, 0x0528, 0x37b3, 0x263a, 0xdecd, 0xcf44,
+    0xfddf, 0xec56, 0x98e9, 0x8960, 0xbbfb, 0xaa72, 0x6306, 0x728f, 0x4014, 0x519d, 0x2522, 0x34ab, 0x0630, 0x17b9, 0xef4e, 0xfec7, 0xcc5c, 0xddd5,
+    0xa96a, 0xb8e3, 0x8a78, 0x9bf1, 0x7387, 0x620e, 0x5095, 0x411c, 0x35a3, 0x242a, 0x16b1, 0x0738, 0xffcf, 0xee46, 0xdcdd, 0xcd54, 0xb9eb, 0xa862,
+    0x9af9, 0x8b70, 0x8408, 0x9581, 0xa71a, 0xb693, 0xc22c, 0xd3a5, 0xe13e, 0xf0b7, 0x0840, 0x19c9, 0x2b52, 0x3adb, 0x4e64, 0x5fed, 0x6d76, 0x7cff,
+    0x9489, 0x8500, 0xb79b, 0xa612, 0xd2ad, 0xc324, 0xf1bf, 0xe036, 0x18c1, 0x0948, 0x3bd3, 0x2a5a, 0x5ee5, 0x4f6c, 0x7df7, 0x6c7e, 0xa50a, 0xb483,
+    0x8618, 0x9791, 0xe32e, 0xf2a7, 0xc03c, 0xd1b5, 0x2942, 0x38cb, 0x0a50, 0x1bd9, 0x6f66, 0x7eef, 0x4c74, 0x5dfd, 0xb58b, 0xa402, 0x9699, 0x8710,
+    0xf3af, 0xe226, 0xd0bd, 0xc134, 0x39c3, 0x284a, 0x1ad1, 0x0b58, 0x7fe7, 0x6e6e, 0x5cf5, 0x4d7c, 0xc60c, 0xd785, 0xe51e, 0xf497, 0x8028, 0x91a1,
+    0xa33a, 0xb2b3, 0x4a44, 0x5bcd, 0x6956, 0x78df, 0x0c60, 0x1de9, 0x2f72, 0x3efb, 0xd68d, 0xc704, 0xf59f, 0xe416, 0x90a9, 0x8120, 0xb3bb, 0xa232,
+    0x5ac5, 0x4b4c, 0x79d7, 0x685e, 0x1ce1, 0x0d68, 0x3ff3, 0x2e7a, 0xe70e, 0xf687, 0xc41c, 0xd595, 0xa12a, 0xb0a3, 0x8238, 0x93b1, 0x6b46, 0x7acf,
+    0x4854, 0x59dd, 0x2d62, 0x3ceb, 0x0e70, 0x1ff9, 0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330, 0x7bc7, 0x6a4e, 0x58d5, 0x495c,
+    0x3de3, 0x2c6a, 0x1ef1, 0x0f78
+    ]
+    
+    crc = 0x0000
+    # print("\nstart")
+    for byte in data:
+        # print(hex(crc), hex(np.ushort(byte)))
+        crc = (np.ushort(crc) >> 8) ^ table[(np.ushort(crc) ^ np.ushort(byte)) & 0x00ff]
+        crc &= 0xffff      
+    # print("end\n")                             
+    return crc
 
 def get_quaternion_from_rotation_matrix(R):
     """Get quaternion representation from a 3x3 rotation matrix representation
@@ -260,11 +316,51 @@ def image_preproc(original_frame, mode=0):
     
     return ret
 
+CAMERA_SERIAL_ID = "usb-xhci-hcd.0.auto-1.4"
+def find_camera_index():
+    cam_idx = -1
+    result = subprocess.run(['v4l2-ctl', '--list-devices'], stdout=subprocess.PIPE) # get camera index by hardware identifier
+    cmd_out = result.stdout.decode('UTF-8')
+    substr_idx = cmd_out.find(CAMERA_SERIAL_ID)
+
+    if substr_idx != -1:
+        start_idx = substr_idx+len(CAMERA_SERIAL_ID)+len(":   ")
+        camera_device = cmd_out[start_idx:(start_idx+len("/dev/videoX"))] # assumes the camera index is less than 10
+        cam_idx = int(camera_device[-1])
+
+    return cam_idx
+
+def find_serial_device():
+    device_name = ""
+    sub_proc_result = subprocess.run(['ls', '/dev'], stdout=subprocess.PIPE)
+    cmd_out = sub_proc_result.stdout.decode('UTF-8')
+    substr_idx = cmd_out.find("ttyACM")
+
+    if substr_idx != -1:
+        start_idx = substr_idx
+        device_name = cmd_out[start_idx:start_idx+len("ttyACMX")]
+
+    return device_name
+        
 def main():
     # -- initialize ROS stuff --
     rospy.init_node("~aruco_cube_detector_node")
     print("--> Node initialized")
-    pose_pub = rospy.Publisher("/aruco_cube_pose", PoseStamped)
+    aruco_pose_pub = rospy.Publisher("/aruco_cube_pose", PoseStamped)
+    head_pose_pub = rospy.Publisher("/head_pose", PoseStamped)
+    
+    # -- open serial port --
+    serial_device_name = find_serial_device()
+    if serial_device_name == "":
+        print("--> Serial device not found!!!")
+        while serial_device_name == "" and not rospy.is_shutdown():
+            print("--> Attempting to connect to serial device...")
+            serial_device_name = find_serial_device()
+            time.sleep(1)
+        print("--> Serial device connected!!!")
+            
+    ser = serial.Serial('/dev/'+serial_device_name, baudrate=460800)  # open serial port
+    print(ser.name)         # check which port was really used
     
     # -- initialize default cube pose --
     aruco_cube_pose = PoseStamped()
@@ -302,11 +398,31 @@ def main():
     print("--> Camera matrix\n{}".format(cam_mat))
     
     # -- initialize frame getter --
-    cap = cv2.VideoCapture(2)
-    ret, test = cap.read()
-    if not ret:
-        print("--> Camera initialization failed!")
-        return
+    # camera_id = find_camera_index()
+    # print("--> Camera index {}".format(camera_id))
+    # if camera_id < 0:
+    #     print("--> Camera not found !!!!!")
+    #     attempt_cnt = 0
+    #     while camera_id < 0 and not rospy.is_shutdown():
+    #         attempt_cnt += 1
+    #         print("--> Trying to re-locate camera, attempt {}".format(attempt_cnt))
+    #         camera_id = find_camera_index()
+    #         time.sleep(1)
+    camera_id = 0
+    cap = cv2.VideoCapture(camera_id)
+    # ret, test = cap.read()
+    # if not ret:
+    #     print("--> Camera initialization failed!")
+    #     attempt_cnt = 0
+        
+    #     while not ret and not rospy.is_shutdown():
+    #         attempt_cnt += 1
+    #         print("--> Trying to re-initialize camera, attempt {}".format(attempt_cnt))
+    #         camera_id = find_camera_index()
+    #         if camera_id >= 0:
+    #             cap = cv2.VideoCapture(camera_id)
+    #             ret, test = cap.read()
+    #         time.sleep(1)
     
     # -- later used for undistort --
     new_cam_mat = cam_mat.copy()
@@ -322,8 +438,11 @@ def main():
     HEAD_ARUCO_ID = 6
     HEAD_ARUCO_SIZE = 20
     HALF_HEAD_ARUCO_SIZE = HEAD_ARUCO_SIZE / 2.0
+    CAMERA_RECONNECTION_PERIOD = 2.75 # seconds
+    SERIAL_DEVICE_RECONNECTION_PERIOD = 1.5 # seconds
     
-    # -- object points for solvePnP --
+    # -- static variables --
+    # object points for solvePnP 
     aruco_cube_aruco_obj_pnts = np.array([[-HALF_ARUCO_CUBE_ARUCO_SIZE, -HALF_ARUCO_CUBE_ARUCO_SIZE, 0], [HALF_ARUCO_CUBE_ARUCO_SIZE, -HALF_ARUCO_CUBE_ARUCO_SIZE, 0], [HALF_ARUCO_CUBE_ARUCO_SIZE, HALF_ARUCO_CUBE_ARUCO_SIZE, 0], [-HALF_ARUCO_CUBE_ARUCO_SIZE, HALF_ARUCO_CUBE_ARUCO_SIZE, 0]]).astype(np.float32)
     head_aruco_obj_pnts= np.array([[-HALF_HEAD_ARUCO_SIZE, -HALF_HEAD_ARUCO_SIZE, 0], [HALF_HEAD_ARUCO_SIZE, -HALF_HEAD_ARUCO_SIZE, 0], [HALF_HEAD_ARUCO_SIZE, HALF_HEAD_ARUCO_SIZE, 0], [-HALF_HEAD_ARUCO_SIZE, HALF_HEAD_ARUCO_SIZE, 0]]).astype(np.float32)
     head_aruco_index = -1
@@ -331,11 +450,49 @@ def main():
     debug_source_id = 5
     canvas = np.zeros((200, 200), np.uint8) # to hold visualization markers
     head_aruco_rvec, head_aruco_tvec, rvec_for_vis, tvec_fin, rvec_for_vis, tvec_for_vis, head_aruco_vis_tvec = None, None, None, None, None, None, None
+    camera_disconnection_counter = 0
+    last_camera_reconnection_attempt_time = time.time()
+    serial_device_disconnection_counter = 0
+    last_serial_device_reconnection_attempt_time = time.time()
     
+    rate = rospy.Rate(1000)
     while not rospy.is_shutdown():
+        rate.sleep()
+        
+        # -- publish data to ros topic for logging --
         aruco_cube_pose.header.stamp = rospy.Time.now()
         aruco_cube_pose.header.seq += 1
-        pose_pub.publish(aruco_cube_pose)
+        aruco_pose_pub.publish(aruco_cube_pose)
+        head_pose.header.stamp = rospy.Time.now()
+        head_pose.header.seq += 1
+        head_pose_pub.publish(head_pose)
+        
+        # -- send information --
+        pose_data = struct.pack('ffffffffffffff', head_pose.pose.position.x, head_pose.pose.position.y, head_pose.pose.position.z \
+            , head_pose.pose.orientation.w, head_pose.pose.orientation.x, head_pose.pose.orientation.y, head_pose.pose.orientation.z \
+                , aruco_cube_pose.pose.position.x, aruco_cube_pose.pose.position.y, aruco_cube_pose.pose.position.z \
+            , aruco_cube_pose.pose.orientation.w, aruco_cube_pose.pose.orientation.x, aruco_cube_pose.pose.orientation.y, aruco_cube_pose.pose.orientation.z)
+        crc = crc16(pose_data)
+        data = struct.pack('ffffffffffffffH', head_pose.pose.position.x, head_pose.pose.position.y, head_pose.pose.position.z \
+            , head_pose.pose.orientation.w, head_pose.pose.orientation.x, head_pose.pose.orientation.y, head_pose.pose.orientation.z \
+                , aruco_cube_pose.pose.position.x, aruco_cube_pose.pose.position.y, aruco_cube_pose.pose.position.z \
+            , aruco_cube_pose.pose.orientation.w, aruco_cube_pose.pose.orientation.x, aruco_cube_pose.pose.orientation.y, aruco_cube_pose.pose.orientation.z \
+                , crc)
+        
+        # -- send serial data, in line with re-connection attempt if disconnected --
+        try:
+            ser.write(data)
+            if serial_device_disconnection_counter > 0:
+                print("--> Serial device reconnected!!")
+                serial_device_disconnection_counter = 0 # device connected
+        except serial.SerialException as e:
+            if (time.time() - last_serial_device_reconnection_attempt_time) > SERIAL_DEVICE_RECONNECTION_PERIOD:
+                last_serial_device_reconnection_attempt_time = time.time()
+                print("--> Serial device disconnected, trying to re-initialize serial device...")
+                serial_device_name = find_serial_device()
+                if serial_device_name != "":
+                    ser = serial.Serial('/dev/'+serial_device_name, baudrate=460800)  # open serial port
+            serial_device_disconnection_counter += 1
         
         # -- read frame in --
         ret, frame = cap.read()
@@ -346,12 +503,26 @@ def main():
                 cv2.drawFrameAxes(canvas, cam_mat, dist, rvec_for_vis, tvec_for_vis, 3.5) # visualization of the orientation of the cube only, at the bottom-left
             if head_aruco_rvec is not None and head_aruco_vis_tvec is not None:
                 cv2.drawFrameAxes(canvas, cam_mat, dist, head_aruco_rvec, head_aruco_vis_tvec, 2.5) # visualization of the orientation of the head only, at the top-left
+            
+            if (time.time() - last_camera_reconnection_attempt_time) > CAMERA_RECONNECTION_PERIOD:
+                last_camera_reconnection_attempt_time = time.time()
+                print("--> Camera disconnected, trying to re-initialize camera...")
+                cap.release()
+                camera_id = find_camera_index()
+                cap = cv2.VideoCapture(camera_id)
+            
+            camera_disconnection_counter += 1
+            
             cv2.imshow("markers", canvas)
-            cv2.imshow("raw", frame)
+            cv2.imshow("raw", np.zeros_like(canvas))
             k = cv2.waitKey(1)
             if k == ord('q'):
                 break
             continue
+        
+        if camera_disconnection_counter > 0:
+            print("--> Camera reconnected!!")
+            camera_disconnection_counter = 0 # frame received, connection is normal
         
         # -- undistortion --
         dst = frame.copy()
@@ -425,13 +596,13 @@ def main():
             head_z =head_aruco_tvec[2][0]
             
             # -- update head pose --
-            aruco_cube_pose.pose.position.x = head_x
-            aruco_cube_pose.pose.position.y = head_y
-            aruco_cube_pose.pose.position.z = head_z
-            aruco_cube_pose.pose.orientation.w = head_w
-            aruco_cube_pose.pose.orientation.x = head_i
-            aruco_cube_pose.pose.orientation.y = head_j
-            aruco_cube_pose.pose.orientation.z = head_k
+            head_pose.pose.position.x = head_x
+            head_pose.pose.position.y = head_y
+            head_pose.pose.position.z = head_z
+            head_pose.pose.orientation.w = head_w
+            head_pose.pose.orientation.x = head_i
+            head_pose.pose.orientation.y = head_j
+            head_pose.pose.orientation.z = head_k
             
             head_aruco_vis_tvec = np.array([[-20.5], [-16.5], [100]], head_aruco_tvec.dtype)
             
